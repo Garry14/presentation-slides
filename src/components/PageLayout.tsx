8d8c070164078bd94b59ac4f7b591c046a66be43
garrydev <garryandi@badr-interactive.com>
2024-04-25 16:15:23 +0700

    fixing next prev

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -16,16 +16,14 @@ const PageLayout: React.FC<PageLayoutProps> = ({
     <div className="p-3 h-screen relative">
       <div className="rounded-lg bg-blue-900 h-full w-full">
         {children}
-        {prevUrl !== undefined && (
+        <div className="absolute w-full bottom-[35px] left-0 flex justify-between items-center p-[50px]">
           <div className="">
-            <Link href={prevUrl}>Prev</Link>
+            {prevUrl !== undefined && <Link href={prevUrl}>Prev</Link>}
           </div>
-        )}
-        {nextUrl !== undefined && (
           <div className="">
-            <Link href={nextUrl}>Next</Link>
+            {nextUrl !== undefined && <Link href={nextUrl}>Next</Link>}
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
