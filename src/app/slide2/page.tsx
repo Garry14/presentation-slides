003f94eb8755215f58781700a75eec5620535210
garrydev <garryandi@badr-interactive.com>
2024-04-25 19:52:32 +0700

    slide 2 okay

--- src/app/slide2/page.tsx
+++ src/app/slide2/page.tsx
@@ -3,9 +3,18 @@ const Slide2 = () => {
   return (
     <PageLayout nextUrl="slide3" prevUrl="/">
       <div className="pt-[125px] px-[78px]">
-        <h1 className="text-[100px] font-mono tracking-tighter leading-tight">
-          Slide 2
+        <h1 className="text-[50px] font-mono tracking-tighter leading-tight">
+          Mengapa Pakai GraphQL?
         </h1>
+        <div className="font-mono text-[30px] mt-[14px]">
+            <ol>
+                <li>1. Response API dikendalikan di sisi klien</li>
+                <li>2. Performance</li>
+                <li>3. Strongly Typed Schema</li>
+                <li>4. Mendukung Realtime</li>
+                <li>5. API Gateway</li>
+            </ol>
+        </div>
       </div>
     </PageLayout>
   );
